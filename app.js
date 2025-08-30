b2d291a52846fb233ad7a65afd10bc2224aa84c6
Mramba-wehu <mrambaedison1@gmail.com>
2025-08-30 08:24:53 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*frontend', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error('Unhandled Error:', err);
+  res.status(500).send('Internal Server Error');
+});
+
+app.listen(5173, () => {
+  console.log('Frontend preview running at http://localhost:5173');
+});
